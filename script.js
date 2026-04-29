e85f6a0bacf3ffc4090ff9723c7c76854be2cdea
Hoeng Mengdy <your_email@gmail.com>
2026-04-29 10:58:21 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,265 @@
+const products = [{
+        name: "Brigh 1L",
+        price: "$14",
+        img: "./img/oka/Brigh 1L.png"
+    },
+    {
+        name: "Bright 3L",
+        price: "$28",
+        img: "./img/oka/bright 3L.png"
+    },
+    {
+        name: "Bright 10L",
+        price: "$42",
+        img: "./img/oka/Bright 10L.png"
+    },
+    {
+        name: "Bright 20L",
+        price: "$55",
+        img: "./img/oka/Bright 20L.png"
+    },
+    {
+        name: "Bright 135",
+        price: "$9",
+        img: "./img/oka/Bright 135.png"
+    },
+    {
+        name: "Oka Lemongrass",
+        price: "$17",
+        img: "./img/oka/Oka-Lemongrass-3L.png"
+    },
+    {
+        name: "Refresh 1L",
+        price: "$9",
+        img: "./img/oka/Refresh 1L.png"
+    },
+    {
+        name: "Refresh 3L",
+        price: "$9",
+        img: "./img/oka/refresh 3L.png"
+    },
+    {
+        name: "Refresh 10L",
+        price: "$9",
+        img: "./img/oka/Refresh 10L.png"
+    },
+    {
+        name: "Refresh 20L",
+        price: "$9",
+        img: "./img/oka/Refresh 20L.png"
+    },
+    {
+        name: "Refresh 135",
+        price: "$9",
+        img: "./img/oka/Refresh 135.png"
+    },
+    {
+        name: "Zuper 3L",
+        price: "$9",
+        img: "./img/oka/Zuper 3L.png"
+    },
+    {
+        name: "Zuper 10L",
+        price: "$9",
+        img: "./img/oka/Zuper 10L.png"
+    },
+    {
+        name: "Zuper 20L",
+        price: "$9",
+        img: "./img/oka/Zuper 20L.png"
+    },
+    {
+        name: "Empress Orange Honey",
+        price: "$14",
+        img: "./img/products Empress/Empress Orange and Honey FA_Mockup.png"
+    },
+    {
+        name: "Pineapple Honey 1000ml",
+        price: "$18",
+        img: "./img/products Empress/Empress_pineapple&honey-1000ml_FA.png"
+    },
+    {
+        name: "Mockup Pineapple Honey",
+        price: "$16",
+        img: "./img/products Empress/Mockup-Pineapple&Honey-800ml FA.png"
+    },
+    {
+        name: "Napa Cabbage Bodywash",
+        price: "$12",
+        img: "./img/products Empress/Napa-cabbage-bodywashV2.png"
+    },
+    {
+        name: "Cabbage Foam 120g",
+        price: "$8",
+        img: "./img/products Empress/Napa-Cabbage-foam120g.png"
+    },
+    {
+        name: "Cabbage Peppermint",
+        price: "$15",
+        img: "./img/products Empress/Napa-Cabbage&Peppermint-800mlpng.png"
+    },
+    {
+        name: "Neem Turmeric Foam",
+        price: "$10",
+        img: "./img/products Empress/Neem&Turmeric-foam120g.png"
+    },
+    {
+        name: "Box Face",
+        price: "$20",
+        img: "./img/products Empress/New-Mockup-Box-face3.png"
+    },
+    {
+        name: "Pouch Cabbage Neem",
+        price: "$6",
+        img: "./img/products Empress/Pouch Cabbage+Neem.png"
+    },
+    {
+        name: "Conditioner Refill",
+        price: "$5",
+        img: "./img/products Empress/POUCH-REFILL_Conditioner-435ml.png"
+    },
+    {
+        name: "Shampoo Refill",
+        price: "$5",
+        img: "./img/products Empress/POUCH-REFILL_Shampoo-450ml.png"
+    },
+    {
+        name: "Mint Cabbage Refill",
+        price: "$4",
+        img: "./img/products Empress/POUCH-refill-720ml-Mint-Cabbage.png"
+    },
+    {
+        name: "Rumdoul Cabbage Refill",
+        price: "$4",
+        img: "./img/products Empress/POUCH-refill-720ml-Rumdoul-Cabbage.png"
+    },
+    {
+        name: "Mint Cabbage Spout",
+        price: "$3",
+        img: "./img/products Empress/Pouch-spout-60ml-Mint-Cabbage.png"
+    },
+    {
+        name: "Rumdoul Cabbage Spout",
+        price: "$3",
+        img: "./img/products Empress/Pouch-spout-60ml-Rumdoul-Cabbage.png"
+    },
+    {
+        name: "Conditioner 30ml",
+        price: "$2",
+        img: "./img/products Empress/Pouch-spout-conditioner-30ml.png"
+    },
+    {
+        name: "Shampoo 35ml",
+        price: "$2",
+        img: "./img/products Empress/Pouch-spout-shampoo-35ml.png"
+    },
+    {
+        name: "Romdoul Rice",
+        price: "$13",
+        img: "./img/products Empress/Romdoul&Rice.png"
+    },
+    {
+        name: "Romdoul Spei",
+        price: "$13",
+        img: "./img/products Empress/Romdoul&spei-Mockup.png"
+    },
+    {
+        name: "Single Mockup",
+        price: "$11",
+        img: "./img/products Empress/Single-mockup.png"
+    },
+    {
+        name: "ក្រែមបន្ទន់សក់រូបរាងថ្មិ ",
+        price: "$11",
+        img: "./img/products Empress/ក្រែមបន្ទន់សក់រូបរាងថ្មិ.png"
+    },
+    {
+        name: "ស្ពៃនិង ស្ដៅ ",
+        price: "$11",
+        img: "./img/products Empress/ស្ពៃនិងស្ដៅ.png"
+    },
+    {
+        name: "ស្ពៃទឹកកថ្មី",
+        price: "$11",
+        img: "./img/products Empress/ស្ពៃទឹកកថ្មី.png"
+    },
+    {
+        name: "ស្ពៃសរីរាង្គរូបរាងថ្មី",
+        price: "$11",
+        img: "./img/products Empress/ស្ពៃសរីរាង្គរូបរាងថ្មី.png"
+    },
+    {
+        name: "Dishwash Pineapple 800ml",
+        price: "$6",
+        img: "./img/RINs/dishwash pineapple 800ml.png"
+    },
+    {
+        name: "Mockup 3L",
+        price: "$10",
+        img: "./img/RINs/Mockup3Lcopypng.png"
+    },
+    {
+        name: "Rin Lemongrass Front",
+        price: "$8",
+        img: "./img/RINs/Rin-Lemongrass-Front-Mock-up2.png"
+    },
+    {
+        name: "Rins Pineapple 1.2L",
+        price: "$7",
+        img: "./img/RINs/Rins-dishwash-Pineapple-1,2L.png"
+    },
+    {
+        name: "Rins Lemongrass 1.2L",
+        price: "$7",
+        img: "./img/RINs/Rins-Dishwashing-lemongrass-1.2L.png"
+    },
+    {
+        name: "Rins Lemongrass 3.5L",
+        price: "$14",
+        img: "./img/RINs/Rins-Dishwashing-Lemongrass-3,5ml.png"
+    },
+    {
+        name: "Rins Pineapple 3.5L",
+        price: "$14",
+        img: "./img/RINs/Rins-Dishwashing-Pineapple-3.5L.png"
+    },
+    {
+        name: "Laundry Orange 1.3L",
+        price: "$9",
+        img: "./img/RINs/Rins-Laundry-Orange-1.3L.png"
+    },
+    {
+        name: "Laundry Orange 5L",
+        price: "$18",
+        img: "./img/RINs/Rins-Laundry-Orange-5L-.png"
+    },
+    {
+        name: "Laundry Purple 1.3L",
+        price: "$9",
+        img: "./img/RINs/Rins-Laundry-Purple-1.3L.png"
+    },
+    {
+        name: "Laundry Purple 5L",
+        price: "$18",
+        img: "./img/RINs/Rins-laundry-Purple-5L-II-.png"
+    }
+
+];
+
+let box = document.getElementById("products");
+
+products.forEach(item => {
+    box.innerHTML += `
+    <div class="card">
+        <div class="p-img">
+            <img src="${item.img}" alt="${item.name}">
+        </div>
+
+        <div class="info">
+            <div class="title">${item.name}</div>
+            <div class="price">❤ ${item.price}</div>
+        </div>
+
+    </div>
+    `;
+});
