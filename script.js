32401e5c8dbe8bb06dc9765d940c7b4a4a265920
Hoeng Mengdy <your_email@gmail.com>
2026-04-30 15:27:29 +0700

    update code

--- script.js
+++ script.js
@@ -1,242 +1,231 @@
 const products = [{
-        name: "Bright 1L",
-        price: "$14",
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា ប្រាយឃ្លីន ១លីត្រ",
+        price: "១០,០០០​៛​ ",
         img: "./img/oka/Brigh 1L.png"
-    },
-    {
-        name: "Bright 3L",
-        price: "$28",
+    }, {
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា ប្រាយឃ្លីន ៣លីត្រ",
+        price: "២៤,០០០​៛​ ",
         img: "./img/oka/bright 3L.png"
     },
     {
-        name: "Bright 10L",
-        price: "$42",
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា ប្រាយឃ្លីន ១០លីត្រ",
+        price: "៤០,០០០​៛​ ",
         img: "./img/oka/Bright 10L.png"
-    },
-    {
-        name: "Bright 20L",
-        price: "$55",
+    }, {
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា ប្រាយឃ្លីន ២០លីត្រ",
+        price: "៧៥,០០០​៛​ ",
         img: "./img/oka/Bright 20L.png"
-    },
-    {
-        name: "Bright 135",
-        price: "$9",
+    }, {
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា ប្រាយឃ្លីន ១៣៥មីលីលីត្រ",
+        price: "៧,០០០​៛​ ",
         img: "./img/oka/Bright 135.png"
-    },
-    {
-        name: "Oka Lemongrass",
-        price: "$17",
+    }, {
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា ក្លិនស្លឹកគ្រៃ ៣លីត្រ",
+        price: "២៤,០០០​៛​ ",
         img: "./img/oka/Oka-Lemongrass-3L.png"
-    },
-    {
-        name: "Refresh 1L",
-        price: "$9",
+    }, {
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា រីហ្វ្រេស ១លីត្រ",
+        price: "១០,០០០​៛​ ",
         img: "./img/oka/Refresh 1L.png"
-    },
-    {
-        name: "Refresh 3L",
-        price: "$9",
+    }, {
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា រីហ្វ្រេស ៣លីត្រ",
+        price: "២៤,០០០​៛​ ",
         img: "./img/oka/refresh 3L.png"
-    },
-    {
-        name: "Refresh 10L",
-        price: "$9",
+    }, {
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា រីហ្វ្រេស ១០លីត្រ",
+        price: "៤០,០០០​៛​ ",
         img: "./img/oka/Refresh 10L.png"
-    },
-    {
-        name: "Refresh 20L",
-        price: "$9",
+    }, {
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា រីហ្វ្រេស ២០លីត្រ",
+        price: "៧៥,០០០​៛​ ",
         img: "./img/oka/Refresh 20L.png"
-    },
-    {
-        name: "Refresh 135",
-        price: "$9",
+    }, {
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា រីហ្វ្រេស 135មីលីលីត្រ",
+        price: "៧,០០០​៛​ ",
         img: "./img/oka/Refresh 135.png"
-    },
-    {
-        name: "Zuper 3L",
-        price: "$9",
+    }, {
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា ស៊ូពើឃ្លីន ៣លីត្រ",
+        price: "២៤,០០០​៛​ ",
         img: "./img/oka/Zuper 3L.png"
     },
     {
-        name: "Zuper 10L",
-        price: "$9",
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា ស៊ូពើឃ្លីន ១០លីត្រ",
+        price: "៤០,០០០​៛​ ",
         img: "./img/oka/Zuper 10L.png"
     },
     {
-        name: "Zuper 20L",
-        price: "$9",
+        name: "សាប៊ូទឹកបោកសម្លៀកបំពាក់ អូកា ស៊ូពើឃ្លីន ២០លីត្រ",
+        price: "៧៥០,០០០​៛​ ",
         img: "./img/oka/Zuper 20L.png"
     },
     {
-        name: "Empress Orange Honey",
-        price: "$14",
+        name: "សាប៊ូកក់សក់ អេមប្រេស ផ្លែក្រូច និង ទឹកឃ្មុំធម្មជាតិ",
+        price: "១៥,០០០​៛​ ",
         img: "./img/products Empress/Empress Orange and Honey FA_Mockup.png"
     },
     {
-        name: "Pineapple Honey 1000ml",
-        price: "$18",
+        name: "សាប៊ូកក់សក់ អេមប្រេស ម្នាស់ និង ទឹកឃ្មុំ ១០០០មីលីលីត្រ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Empress_pineapple&honey-1000ml_FA.png"
     },
     {
-        name: "Mockup Pineapple Honey",
-        price: "$16",
+        name: "សាប៊ូកក់សក់ អេមប្រេស ម្នាស់ និង ទឹកឃ្មុំ ៨០០មីលីលីត្រ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Mockup-Pineapple&Honey-800ml FA.png"
     },
     {
-        name: "Napa Cabbage Bodywash",
-        price: "$12",
+        name: "សាប៊ូដុសខ្លួន អេមប្រេស ស្ពៃក្រញាញ់",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Napa-cabbage-bodywashV2.png"
     },
     {
-        name: "Cabbage Foam 120g",
-        price: "$8",
+        name: "ហ្វូមលាងមុខ ស្ពៃក្រញាញ់ ១២០ក្រាម",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Napa-Cabbage-foam120g.png"
     },
     {
-        name: "Cabbage Peppermint",
-        price: "$15",
+        name: "សាប៊ូកក់សក់ ស្ពៃក្រញាញ់",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Napa-Cabbage&Peppermint-800mlpng.png"
     },
     {
-        name: "Neem Turmeric Foam",
-        price: "$10",
+        name: "ហ្វូមលាងមុខ ស្ដៅ និង រមៀត ១២០ក្រាម",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Neem&Turmeric-foam120g.png"
     },
     {
-        name: "Box Face",
-        price: "$20",
+        name: "ប្រអប់ផលិតផលថែរក្សាមុខ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/New-Mockup-Box-face3.png"
     },
     {
-        name: "Pouch Cabbage Neem",
-        price: "$6",
+        name: "សាប៊ូកក់សក់ស្ពៃក្រញាញ់ និង ស្ដៅ កញ្ចប់បំពេញ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/PouchCabbageNeem.png"
     },
     {
-        name: "Conditioner Refill",
-        price: "$5",
+        name: "ក្រែមបន្ទន់សក់ កញ្ចប់បំពេញ ៤៣៥មីលីលីត្រ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/POUCH-REFILL_Conditioner-435ml.png"
     },
     {
-        name: "Shampoo Refill",
+        name: "សាប៊ូកក់សក់ កញ្ចប់បំពេញ ៤៥០មីលីលីត្រ",
         price: "$5",
         img: "./img/products Empress/POUCH-REFILL_Shampoo-450ml.png"
     },
     {
-        name: "Mint Cabbage Refill",
-        price: "$4",
+        name: "សាប៊ូកក់សក់ មិនท์ និង ស្ពៃក្រញាញ់ កញ្ចប់បំពេញ ៧២០មីលីលីត្រ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/POUCH-refill-720ml-Mint-Cabbage.png"
     },
     {
-        name: "Rumdoul Cabbage Refill",
-        price: "$4",
+        name: "សាប៊ូកក់សក់ រំដួល និង ស្ពៃក្រញាញ់ កញ្ចប់បំពេញ ៧២០មីលីលីត្រ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/POUCH-refill-720ml-Rumdoul-Cabbage.png"
     },
     {
-        name: "Mint Cabbage Spout",
-        price: "$3",
+        name: "សាប៊ូកក់សក់ មិនท์ និង ស្ពៃក្រញាញ់ កញ្ចប់តូច ៦០មីលីលីត្រ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Pouch-spout-60ml-Mint-Cabbage.png"
     },
     {
-        name: "Rumdoul Cabbage Spout",
-        price: "$3",
+        name: "សាប៊ូកក់សក់ រំដួល និង ស្ពៃក្រញាញ់ កញ្ចប់តូច ៦០មីលីលីត្រ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Pouch-spout-60ml-Rumdoul-Cabbage.png"
     },
     {
-        name: "Conditioner 30ml",
-        price: "$2",
+        name: "ក្រែមបន្ទន់សក់ កញ្ចប់តូច ៣០មីលីលីត្រ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Pouch-spout-conditioner-30ml.png"
     },
     {
-        name: "Shampoo 35ml",
-        price: "$2",
+        name: "សាប៊ូកក់សក់ កញ្ចប់តូច ៣៥មីលីលីត្រ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Pouch-spout-shampoo-35ml.png"
     },
     {
-        name: "Romdoul Rice",
-        price: "$13",
+        name: "សាប៊ូកក់សក់ រំដួល និង អង្ករ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Romdoul&Rice.png"
     },
     {
-        name: "Romdoul Spei",
-        price: "$13",
+        name: "សាប៊ូកក់សក់ រំដួល និង ស្ពៃក្រញាញ់",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Romdoul&spei-Mockup.png"
     },
     {
-        name: "Single Mockup",
-        price: "$11",
+        name: "សាប៊ូកក់សក់ អេមប្រេស",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/Single-mockup.png"
     },
     {
         name: "ក្រែមបន្ទន់សក់រូបរាងថ្មី",
-        price: "$11",
+        price: "១៥,០០០​៛​ ",
         img: "./img/products Empress/ក្រែមបន្ទន់សក់រូបរាងថ្មិ.png"
     },
     {
-        name: "ស្ពៃនិង ស្ដៅ",
-        price: "$11",
+        name: "សាប៊ូកក់សក់ ស្ពៃ និង ស្ដៅ",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/ស្ពៃនិងស្ដៅ.png"
     },
     {
-        name: "ស្ពៃទឹកកថ្មី",
-        price: "$11",
+        name: "សាប៊ូកក់សក់ ស្ពៃទឹកកថ្មី",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/ស្ពៃទឹកកថ្មី.png"
     },
     {
-        name: "ស្ពៃសរីរាង្គរូបរាងថ្មី",
-        price: "$11",
+        name: "សាប៊ូកក់សក់ ស្ពៃសរីរាង្គរូបរាងថ្មី",
+        price: "១០,០០០​៛​ ",
         img: "./img/products Empress/ស្ពៃសរីរាង្គរូបរាងថ្មី.png"
     },
     {
-        name: "Dishwash Pineapple 800ml",
-        price: "$6",
+        name: "ទឹកលាងចាន រីន ម្នាស់ ៨០០មីលីលីត្រ",
+        price: "៥,០០០​៛​ ",
         img: "./img/RINs/dishwash pineapple 800ml.png"
     },
     {
-        name: "Mockup 3L",
-        price: "$10",
+        name: "ទឹកលាងចាន រីន ៣លីត្រ",
+        price: "៥,០០០​៛​ ",
         img: "./img/RINs/Mockup3Lcopypng.png"
     },
     {
-        name: "Rin Lemongrass Front",
-        price: "$8",
+        name: "ទឹកលាងចាន រីន ក្លិនស្លឹកគ្រៃ",
+        price: "៥,០០០​៛​ ",
         img: "./img/RINs/Rin-Lemongrass-Front-Mock-up2.png"
     },
-
     {
-        name: "Rins Lemongrass 1.2L",
-        price: "$7",
+        name: "ទឹកលាងចាន រីន ស្លឹកគ្រៃ ១.២លីត្រ",
+        price: "៥,០០០​៛​ ",
         img: "./img/RINs/Rins-Dishwashing-lemongrass-1.2L.png"
     },
     {
-        name: "Rins Lemongrass 3.5L",
-        price: "$14",
+        name: "ទឹកលាងចាន រីន ស្លឹកគ្រៃ ៣.៥លីត្រ",
+        price: "៥,០០០​៛​ ",
         img: "./img/RINs/Rins-Dishwashing-Lemongrass-3,5ml.png"
     },
     {
-        name: "Rins Pineapple 3.5L",
-        price: "$14",
+        name: "ទឹកលាងចាន រីន ម្នាស់ ៣.៥លីត្រ",
+        price: "៥,០០០​៛​ ",
         img: "./img/RINs/Rins-Dishwashing-Pineapple-3.5L.png"
     },
     {
-        name: "Laundry Orange 1.3L",
-        price: "$9",
+        name: "ទឹកបោកសម្លៀកបំពាក់ រីន ក្លិនក្រូច ១.៣លីត្រ",
+        price: "៥,០០០​៛​ ",
         img: "./img/RINs/Rins-Laundry-Orange-1.3L.png"
     },
     {
-        name: "Laundry Orange 5L",
-        price: "$18",
+        name: "ទឹកបោកសម្លៀកបំពាក់ រីន ក្លិនក្រូច ៥លីត្រ",
+        price: "៥,០០០​៛​ ",
         img: "./img/RINs/Rinslaundry5L.png"
     },
     {
-        name: "Laundry Purple 1.3L",
-        price: "$9",
+        name: "ទឹកបោកសម្លៀកបំពាក់ រីន ក្លិនផ្កា ១.៣លីត្រ",
+        price: "៥,០០០​៛​ ",
         img: "./img/RINs/Rins-Laundry-Purple-1.3L.png"
     },
     {
-        name: "Laundry Purple 5L",
-        price: "$18",
+        name: "ទឹកបោកសម្លៀកបំពាក់ រីន ក្លិនផ្កា ៥លីត្រ",
+        price: "៥,០០០​៛​ ",
         img: "./img/RINs/Rins-laundry-Purple-5L-II-.png"
     }
 ];
@@ -245,7 +234,7 @@ let box = document.getElementById("products");
 
 products.forEach(item => {
     box.innerHTML += `
-        <div class="card">
+        <div class="card Kantumruy">
             <div class="p-img">
                 <img src="${item.img}" alt="${item.name}">
             </div>
